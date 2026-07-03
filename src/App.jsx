8980e526ded96a018cb620d2cd2e10451f24f11e
Gautam Ji <gautam123@gmail.com>
2026-07-03 17:22:15 +0530

    setup of monaco editor

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
+import React from "react";
+import { Editor } from "@monaco-editor/react";
 
 const App = () => {
   return (
-    <div className='h-screen w-full bg-gray-950 flex gap-4'>
-          <aside className='h-full w-1/5 bg-amber-50 rounded-lg' >
-           
-          </aside>
-          <section className=' w-3/4 bg-neutral-800 rounded-lg' >
-
-          </section>
+    <div className="h-screen w-full bg-gray-950 flex gap-4">
+      <aside className="h-full w-1/5 bg-amber-50 rounded-lg"></aside>
+      <section className=" w-3/4 bg-neutral-800 rounded-lg">
+        <Editor
+        height="100%"
+        defaultLanguage="javascript"
+        defaultValue="// Write your code here"
+        theme="vs-dark"
+      />
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
