230c316333da26573277def4929163fb7b92e24f
Gautam Ji <gautam123@gmail.com>
2026-07-03 17:05:04 +0530

    basic setup of ui

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='h-screen w-full bg-gray-950 flex gap-4'>
+          <aside className='h-full w-1/5 bg-amber-50 rounded-lg' >
+           
+          </aside>
+          <section className=' w-3/4 bg-neutral-800 rounded-lg' >
+
+          </section>
+    </div>
   )
 }
 
